103ae74c0b23dbd59662b9d7e09bb8d5e981fe64
Spiivak <edenspiivak@gmail.com>
2023-11-15 15:47:03 +0200

    Fixed some bugs Updated check for sizes Added getRandomColor

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 'use strict'
 
 function onBallClick(elBall) {
-    var currentWidth = parseInt(elBall.style.width, 10) || 0;
+    var currentWidth = parseInt(elBall.style.width, 10) || 100
+    var currentHeight = parseInt(elBall.style.height, 10) || 100
 
-    if (currentWidth === 400) {
-        elBall.style.width = 100 + 'px';
+    console.log('currentWidth', currentWidth)
+    console.log('currentHeight', currentHeight)
+
+    if (currentWidth >= 400 && currentHeight >= 400) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.style.backgroundColor = 'lightcoral'
+
     } else {
-        elBall.style.width = getRandomInt(20, 60) + 'px';
+        var newWidth = currentWidth + getRandomInt(20, 60)
+        elBall.style.width = newWidth + 'px'
+        elBall.style.height = newWidth + 'px'
+        elBall.style.backgroundColor = getRandomColor()
     }
-
 }
