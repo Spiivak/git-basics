7bf0dd0e3398abd709fd0499b1d7348a82deeca1
Spiivak <edenspiivak@gmail.com>
2023-11-15 15:23:08 +0200

    Update ball width to increase randomaly

--- js/main.js
+++ js/main.js
@@ -2,18 +2,11 @@
 
 function onBallClick(elBall) {
     var currentWidth = parseInt(elBall.style.width, 10) || 0;
-    var currentHeight = parseInt(elBall.style.height, 10) || 0;
 
-    if (currentWidth === 400 && currentHeight === 400) {
-
+    if (currentWidth === 400) {
         elBall.style.width = 100 + 'px';
-        elBall.style.height = 100 + 'px';
-
     } else {
-
-
-        elBall.style.width = (currentWidth + 50) + 'px';
-        elBall.style.height = (currentHeight + 50) + 'px';
+        elBall.style.width = getRandomInt(20, 60) + 'px';
     }
 
 }
