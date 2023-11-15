42d4bf7a2691b16d3a34baf7d16e2bbf7da13871
Spiivak <edenspiivak@gmail.com>
2023-11-15 15:03:56 +0200

    Added limit to ball sizes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ function onBallClick(elBall) {
     var currentWidth = parseInt(elBall.style.width, 10) || 0;
     var currentHeight = parseInt(elBall.style.height, 10) || 0;
 
-    elBall.style.width = (currentWidth + 50) + 'px';
-    elBall.style.height = (currentHeight + 50) + 'px';
+    if (currentWidth === 400 && currentHeight === 400) {
+
+        elBall.style.width = 100 + 'px';
+        elBall.style.height = 100 + 'px';
+
+    } else {
+
+
+        elBall.style.width = (currentWidth + 50) + 'px';
+        elBall.style.height = (currentHeight + 50) + 'px';
+    }
 
 }
