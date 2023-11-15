2a098f2a554e581f33e02764b9dd619d4db141d9
Spiivak <edenspiivak@gmail.com>
2023-11-15 14:52:35 +0200

    Fixed typo in html Updated the function of the ball

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict'
 
-function onBallClick() {
-    console.log('Ball clicked')
+function onBallClick(elBall) {
+    var currentWidth = parseInt(elBall.style.width, 10) || 0;
+    var currentHeight = parseInt(elBall.style.height, 10) || 0;
+
+    elBall.style.width = (currentWidth + 50) + 'px';
+    elBall.style.height = (currentHeight + 50) + 'px';
+
 }
